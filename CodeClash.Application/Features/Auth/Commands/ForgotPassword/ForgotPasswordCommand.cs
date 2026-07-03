66a8c726939f7d98a445ee9bdd583b2ea2d60276
majid <majidvp501@gmail.com>
2026-07-03 12:14:03 +0530

    The Forgot psd Command Added

---
+++ CodeClash.Application/Features/Auth/Commands/ForgotPassword/ForgotPasswordCommand.cs
@@ -0,0 +1,7 @@
+﻿using CodeClash.Application.Common.Models;
+using CodeClash.Application.Features.Auth.DTOs;
+using MediatR;
+
+namespace CodeClash.Application.Features.Auth.Commands.ForgotPassword;
+
+public record ForgotPasswordCommand(ForgotPasswordRequestDto Dto) : IRequest<Result<string>>;

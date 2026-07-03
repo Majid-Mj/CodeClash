319ffb9d4f90864fc2b2c8df4d2d7b8eee2696ac
majid <majidvp501@gmail.com>
2026-07-03 12:17:42 +0530

    The Forgot psd Validator Implimeted

---
+++ CodeClash.Application/Features/Auth/Commands/ForgotPassword/ForgotPasswordCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CodeClash.Application.Features.Auth.Commands.ForgotPassword;
+
+public class ForgotPasswordCommandValidator : AbstractValidator<ForgotPasswordCommand>
+{
+    public ForgotPasswordCommandValidator()
+    {
+        RuleFor(x => x.Dto.Email)
+            .NotEmpty().WithMessage("Email address is required.")
+            .EmailAddress().WithMessage("A valid email address is required.");
+    }
+}

dbd773b6f50951701a1787f6229257b7164eacdd
xalmanfaris <xalmanfarisp@gmail.com>
2026-07-11 23:29:37 +0530

    fix: update AddBattleRecordsTable migration Up/Down methods to create/drop table

--- CodeClash.Infrastructure/Migrations/20260711172655_AddBattleRecordsTable.cs
+++ CodeClash.Infrastructure/Migrations/20260711172655_AddBattleRecordsTable.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,13 +10,43 @@ namespace CodeClash.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "BattleRecords",
+                columns: table => new
+                {
+                    Id = table.Column<System.Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<System.Guid>(type: "uniqueidentifier", nullable: false),
+                    OpponentName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ProblemName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Duration = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    IsWin = table.Column<bool>(type: "bit", nullable: false),
+                    EloChange = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<System.DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BattleRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BattleRecords_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
+            migrationBuilder.CreateIndex(
+                name: "IX_BattleRecords_UserId",
+                table: "BattleRecords",
+                column: "UserId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "BattleRecords");
         }
     }
 }
